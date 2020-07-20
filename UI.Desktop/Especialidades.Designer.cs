63f7264669d6d407081fbd981df40b4908afa2f5
fabbrilu <fabbrilu@users.noreply.github.com>
2020-07-20 01:04:07 -0300

    Cambios de ventana especialidades

---
+++ UI.Desktop/Especialidades.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace UI.Desktop
+{
+    partial class Especialidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnActu = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.tlEspecialidades = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvEspecialidades = new System.Windows.Forms.DataGridView();
+            this.tsEspecialidades = new System.Windows.Forms.ToolStrip();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.tlEspecialidades.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEspecialidades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnActu
+            // 
+            this.btnActu.Location = new System.Drawing.Point(553, 365);
+            this.btnActu.Name = "btnActu";
+            this.btnActu.Size = new System.Drawing.Size(75, 23);
+            this.btnActu.TabIndex = 0;
+            this.btnActu.Text = "Actualizar";
+            this.btnActu.UseVisualStyleBackColor = true;
+            this.btnActu.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(648, 365);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.tlEspecialidades);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.tsEspecialidades);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.btnActu);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.btnSalir);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(762, 377);
+            this.toolStripContainer1.ContentPanel.Load += new System.EventHandler(this.toolStripContainer1_ContentPanel_Load);
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "tsEspecialidades";
+            this.toolStripContainer1.Size = new System.Drawing.Size(762, 402);
+            this.toolStripContainer1.TabIndex = 2;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+           
+            // 
+            // tlEspecialidades
+            // 
+            this.tlEspecialidades.ColumnCount = 2;
+            this.tlEspecialidades.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlEspecialidades.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlEspecialidades.Controls.Add(this.dgvEspecialidades, 0, 0);
+            this.tlEspecialidades.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlEspecialidades.Location = new System.Drawing.Point(0, 0);
+            this.tlEspecialidades.Name = "tlEspecialidades";
+            this.tlEspecialidades.RowCount = 2;
+            this.tlEspecialidades.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlEspecialidades.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlEspecialidades.Size = new System.Drawing.Size(762, 377);
+            this.tlEspecialidades.TabIndex = 4;
+            // 
+            // dgvEspecialidades
+            // 
+            this.dgvEspecialidades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tlEspecialidades.SetColumnSpan(this.dgvEspecialidades, 2);
+            this.dgvEspecialidades.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvEspecialidades.Location = new System.Drawing.Point(3, 3);
+            this.dgvEspecialidades.Name = "dgvEspecialidades";
+            this.dgvEspecialidades.Size = new System.Drawing.Size(756, 182);
+            this.dgvEspecialidades.TabIndex = 0;
+            // 
+            // tsEspecialidades
+            // 
+            this.tsEspecialidades.Dock = System.Windows.Forms.DockStyle.None;
+            this.tsEspecialidades.Location = new System.Drawing.Point(3, 0);
+            this.tsEspecialidades.Name = "tsEspecialidades";
+            this.tsEspecialidades.Size = new System.Drawing.Size(111, 25);
+            this.tsEspecialidades.TabIndex = 3;
+            this.tsEspecialidades.Text = "toolStrip1";
+            // 
+            // Especialidades
+            // 
+            this.ClientSize = new System.Drawing.Size(761, 400);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Especialidades";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.tlEspecialidades.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEspecialidades)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+       
+        
+        private System.Windows.Forms.ToolStrip esEspecialidades;
+        private System.Windows.Forms.DataGridView dgvEspecialidades;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.ToolStripButton tbsNuevo;
+        private System.Windows.Forms.ToolStripButton tbsEditar;
+        private System.Windows.Forms.ToolStripButton tbsEliminar;
+        private System.Windows.Forms.Button btnActu;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip tsEspecialidades;
+        private System.Windows.Forms.TableLayoutPanel tlEspecialidades;
+       
+    }
+}
---
+++ UI.Desktop/Especialidades.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Logic;
+using Business.Entities;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UI.Desktop
+{
+    public partial class Especialidades : Form
+    {
+        public Especialidades()
+        {
+            InitializeComponent();
+            this.dgvEspecialidades.AutoGenerateColumns = false;
+        }
+
+        private void Especialidades_Load(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        public void Listar()
+        {
+            EspecialidadLogic ul = new EspecialidadLogic();
+            this.dgvEspecialidades.DataSource = ul.GetAll();
+        }
+
+       
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void tbsNuevo_Click(object sender, EventArgs e)
+        {
+            EspecialidadesDesktop ud = new EspecialidadesDesktop(ApplicationForm.ModoForm.Alta);
+            ud.ShowDialog();
+            this.Listar();
+        }
+
+        private void tbsEditar_Click(object sender, EventArgs e)
+        {
+            if (this.dgvEspecialidades.SelectedRows != null)
+            {
+                string descripcion = ((Business.Entities.Especialidad)this.dgvEspecialidades.SelectedRows[0].DataBoundItem).Descripcion;
+                EspecialidadesDesktop ed = new EspecialidadesDesktop(descripcion, ApplicationForm.ModoForm.Modificacion);
+                ed.ShowDialog();
+                this.Listar();
+            }
+        }
+
+        private void tbsEliminar_Click(object sender, EventArgs e)
+        {
+            if (this.dgvEspecialidades.SelectedRows != null)
+            {
+                string  descripcion  = ((Business.Entities.Especialidad)this.dgvEspecialidades.SelectedRows[0].DataBoundItem).Descripcion;
+                EspecialidadesDesktop ed = new EspecialidadesDesktop(descripcion, ApplicationForm.ModoForm.Baja);
+                ed.ShowDialog();
+                this.Listar();
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
