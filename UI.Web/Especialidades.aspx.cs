aceedf8003b7fcaf1b911bf3389d6cee704d7e24
fabbrilu <fabbrilu@users.noreply.github.com>
2020-09-12 15:38:53 -0300

    metodos especialidades

--- UI.Web/Especialidades.aspx.cs
+++ UI.Web/Especialidades.aspx.cs
@@ -75,6 +75,131 @@ namespace UI.Web
         }
         #endregion
 
+        #region Metodos
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.ID.Text = this.Entity.ID;
+            this.descripcionTextBox.Text = this.Entity.Descripcion;
+        }
 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.LoadGrid();
+        }
+
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+
+        }
+
+        private void ClearForm()
+        {
+            this.idTextBox.Text = string.Empty;
+            this.descripcionTextBox.Text = string.Empty;
+        
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.idTextBox.Enabled = enable;
+            this.descripcionTextBox.Enabled = enable;
+           
+        }
+
+        protected void gridView_selectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadEntity(Especialidad especialidad)
+        {
+            especialidad.ID = this.idTextBox.Text;
+           especialidad.Descripcion = this.descripcionTextBox.Text;
+
+        }
+
+        private void SaveEntity(Especialidad especialidad)
+        {
+            this.Logic.Save(especialidad);
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    
+                        this.DeleteEntity(this.SelectedID);
+                        this.formPanel.Visible = false;
+                        this.gridView.DataBind();
+
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Especialidad();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                        this.SaveEntity(this.Entity);
+                        this.gridView.DataBind();
+                        this.formPanel.Visible = false;
+                    break;
+                case FormModes.Alta:
+                    
+                        this.Entity = new Especialidad();
+                        this.LoadEntity(this.Entity);
+                        this.SaveEntity(this.Entity);
+                        this.gridView.DataBind();
+                        this.formPanel.Visible = false;
+                    break;
+            }
+            this.formPanel.Visible = false;
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.isEntitySelected)
+            {
+                this.EnableForm(true);
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.isEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = false;
+            gridView.DataBind();
+        }
+
+        #endregion
     }
+
 }
