09c332d93b3b9b71cffbb2f3d09eecadadb9e55d
tomasvivas <tomiagus00@gmail.com>
2020-11-06 18:21:24 -0300

    algo de inscripciones

--- Business.Logic/InscripcionLogic.cs
+++ Business.Logic/InscripcionLogic.cs
@@ -10,6 +10,26 @@ namespace Business.Logic
 {
     public class InscripcionLogic
     {
+        public InscripcionLogic()
+        {
+            cursoAdapter = new CursoAdapter();
+            ins = new InscripcionAdapter();
+        }
+
+        private CursoAdapter _cursoAdapter;
+        public CursoAdapter cursoAdapter
+        {
+            get { return _cursoAdapter; }
+            set { _cursoAdapter = value; }
+        }
+
+        private InscripcionAdapter _ins;
+        public InscripcionAdapter ins
+        {
+            get { return _ins; }
+            set { _ins = value; }
+        }
+
         public List<AlumnoInscripcion> GetAll(int id)
         {
             InscripcionAdapter ia = new InscripcionAdapter();
@@ -22,10 +42,17 @@ namespace Business.Logic
             return ia.GetOne(id);
         }
 
-        public List<Curso> GetCursosAlumno(int id)
+        public List<Curso> GetCursosAlumno(Persona p)
         {
-            InscripcionAdapter ia = new InscripcionAdapter();
-            return ia.GetCursosAlumno(id);
+            List<Curso> cursos = new List<Curso>();
+            List<Curso> disp = new List<Curso>();
+            List<AlumnoInscripcion> inscripciones = new List<AlumnoInscripcion>();
+            InscripcionAdapter ins = new InscripcionAdapter();
+            inscripciones = ins.GetInscripciones(p.ID);
+            cursos = cursoAdapter.GetAll();
+            disp = (from Curso in cursos where (Curso.AnioCalendario == DateTime.Now.Year && Curso.Cupo > 0 && inscripciones.All(p2 => p2.IDCurso != Curso.ID)) select Curso).ToList();
+            return disp;
+
         }
 
         public void Save(AlumnoInscripcion ins)
